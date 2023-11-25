e8290274de84db7e3112728ee2c5e48678887204
miguelvega <miguelvegasolde@gmail.com>
2023-11-25 12:25:00 -0500

    Modificaciones para la parte1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,7 +5,8 @@ class MoviesController < ApplicationController
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
-    # will render render app/views/movies/show.html.haml by default
+    render(:partial => 'movie', :object => @movie) if request.xhr?
+    # will render app/views/movies/show.<extension> by default
   end
   def new
     @movie = Movie.new
